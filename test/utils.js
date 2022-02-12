169643c60722a0cf95d1c691b43b4a848b9efc5f
Stuart Minchington <stuart@Stuarts-MoxoBook-Pro.local>
2022-02-11 16:40:01 -0800

    Uploaded results

---
+++ test/utils.js
@@ -0,0 +1,23 @@
+const brokenCapabilities = {
+    'browserName': 'googlechrome',
+    'platformName': 'macOS 10.15',
+    'browserVersion': 'latest',
+    'sauce:options': {
+        'name': 'Broken Google Search',
+        'screenResolution': '1280x960'
+    }
+};
+
+const workingCapabilities = {
+    'browserName': 'googlechrome',
+    'platformName': 'macOS 10.15',
+    'browserVersion': 'latest',
+    'sauce:options': {
+        'name': 'Guinea-Pig Sauce',
+        'screenResolution': '1280x960',
+        'tags': ['Neo']
+    }
+};
+
+exports.brokenCapabilities = brokenCapabilities
+exports.workingCapabilities = workingCapabilities
